8f56caa506bd5c1a6bb4f33ac38cd429c77c8b00
l499477004 <35189123+l499477004@users.noreply.github.com>
2020-12-05 16:00:12 +0800

    update

--- jd_small_home.js
+++ jd_small_home.js
@@ -5,15 +5,13 @@
  * @Last Modified time: 2020-11-23 12:27:20
  */
 /*
-东东小窝 https://raw.githubusercontent.com/l499477004/JD-scripts/master/jd_small_home.js
+东东小窝 https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_small_home.js
 现有功能：
 做日常任务任务，每日抽奖（有机会活动京豆，使用的是免费机会，不消耗WO币）
 助力好友：一个账号一天只能助力一次(即：每个人助力机会只有一次)
 后期有空优化相互助力功能
 TODO；装扮领京豆（使用WO币购买装饰品可以获得京豆，分别可获得5,20，50,100,200,400,700，1200京豆）
-
 注：目前使用此脚本会给脚本内置的两个码进行助力，请知晓
-
 APP活动入口：
 京东APP首页 ->搜索 玩一玩 -> DIY理想家
 或 京东APP -> 我的-> 游戏与更多 - > 东东小窝
@@ -21,23 +19,19 @@ APP活动入口：
 来客有礼 - > 首页 -> 东东小窝
 网页入口（注：进入后不能再此刷新，否则会有问题，需重新输入此链接进入）
 https://h5.m.jd.com/babelDiy/Zeus/2HFSytEAN99VPmMGZ6V4EYWus1x/index.html
-
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
 ===============Quantumultx===============
 [task_local]
 #东东小窝
-16 0 * * * https://raw.githubusercontent.com/l499477004/JD-scripts/master/jd_small_home.js, tag=东东小窝, img-url=https://raw.githubusercontent.com/58xinian/icon/master/ddxw.png  enabled=true
-
+16 0 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_small_home.js, tag=东东小窝, img-url=https://raw.githubusercontent.com/58xinian/icon/master/ddxw.png  enabled=true
 ================Loon==============
 [Script]
-cron "16 0 * * *" script-path=https://raw.githubusercontent.com/l499477004/JD-scripts/master/jd_small_home.js, tag=东东小窝
-
+cron "16 0 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_small_home.js, tag=东东小窝
 ===============Surge=================
-东东小窝 = type=cron,cronexp="16 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/l499477004/JD-scripts/master/jd_small_home.js
-
+东东小窝 = type=cron,cronexp="16 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_small_home.js
 ============小火箭=========
-东东小窝 = type=cron,script-path=https://raw.githubusercontent.com/l499477004/JD-scripts/master/jd_small_home.js, cronexpr="16 0 * * *", timeout=200, enable=true
+东东小窝 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_small_home.js, cronexpr="16 0 * * *", timeout=200, enable=true
  */
 const $ = new Env('东东小窝');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -157,10 +151,10 @@ async function doChannelsListTask(taskId, taskType) {
 }
 async function helpFriends() {
   await updateInviteCode();
-  if (!$.updatePkActivityIdRes) await updateInviteCodeCDN();
+  if (!$.inviteCodes) await updateInviteCodeCDN();
   for (let item of $.inviteCodes.inviteCode) {
     if (!item) continue
-    await createAssistUser(item, $.createAssistUserID || "1318106976846299138");
+    await createAssistUser(item, $.createAssistUserID || "1334893633204080641");
   }
 }
 async function doAllTask() {
@@ -710,7 +704,7 @@ function updateInviteCode(url = 'https://raw.githubusercontent.com/lxk0301/updat
     })
   })
 }
-function updateInviteCodeCDN(url = 'https://cdn.jsdelivr.net/gh/lxk0301/updateTeam@master/jd_updateSmallHomeInviteCode.json') {
+function updateInviteCodeCDN(url = 'https://raw.fastgit.org/lxk0301/updateTeam/master/jd_updateSmallHomeInviteCode.json') {
   return new Promise(resolve => {
     $.get({url}, async (err, resp, data) => {
       try {
