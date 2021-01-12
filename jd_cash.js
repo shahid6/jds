ce101f7acd844df3e4d423adb97be8a89f90d543
l499477004 <35189123+l499477004@users.noreply.github.com>
2021-01-12 11:04:46 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -26,8 +26,8 @@ let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
 const randomCount = $.isNode() ? 20 : 5;
 const inviteCodes = [
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`
+  `JxI-aOu1ZvwinjM`,
+  `JxI-aOu1ZvwinjM`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
